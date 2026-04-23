d503ecd7bd1d7a3d9374b9a906ffa59e1cc76d02
Sudhanshi Garg <sudhanshigarg13@gmail.com>
2026-04-23 09:31:21 +0530

    added 1440B

--- 1440B.cpp
+++ 1440B.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
+#include<vector>
 using namespace std;
 
 void solve() {
-    int n; cin >> n;
+    long long n; cin >> n;
+    long long k; cin >> k;
+    long long m = n*k;
+    vector<long long> vec(m);
+
+    for(int i=0; i<m; i++){
+        cin >> vec[i];
+    }
+
+    long long median = 0;
+    long long b = n/2;
+    long long end = m-b-1;
+    
+    while(k--){
+        median += vec[end];
+        end -= (b+1);
+    }
+
+    cout << median << endl;
 }
 
 int main() {
