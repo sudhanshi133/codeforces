19f550f661833929daabf42f61bbf419c88ae4a8
Sudhanshi Garg <sudhanshigarg13@gmail.com>
2026-04-25 13:17:03 +0530

    1593B

--- 1593B.cpp
+++ 1593B.cpp
@@ -1,39 +1,67 @@
 #include<iostream>
 #include<algorithm>
-#include<stdio.h>
 #include<string>
 using namespace std;
+
+long long func(string s, long long index1, char a, char b){
+        for(long long i=index1-1; i>=0; i--){
+        if(s[i] == a || s[i] == b){
+            return i;
+        }
+    }
+    return 0;  
+}
+
+long long funcA(string s){
+    long long index1;
+    bool flag = false;
+     for(long long i=s.length()-1; i>=0; i--){
+        if(s[i] == '0'){
+            index1 = i;
+            flag = true;
+            break;
+        }
+    }  
+   if(flag){
+    long long index2 = func(s, index1, '5', '0');
+    long long reduced = s.length() - index2 - 2;
+    return reduced;
+   }
+   return INT_MAX;
+}
 
 void solve() {
     long long n; cin >> n;
     string s = to_string(n);
-    
-    int maxi = 0;
-    int start = s.size()- 1;
+    bool flag = false;
+    long long index1 = 0;
 
-    while(start >= 0){
-        if(s[start] == '5' || s[start] == '0'){
+    for(long long i=s.length()-1; i>=0; i--){
+        if(s[i] == '0'){
+            index1 = i;
+            flag = true;
             break;
         }
-            start--;
+        else if(s[i] == '5'){
+            index1 = i;
+            break;
         }
-
-    int end = start - 1;
-    while(end >= 0){
-        string k;
-        k+= s[start];
-        k+= s[end];
-
-        if(k.size() > 0){
-        int m = stoi(k);
-       if(m % 25 == 0){
-        break;
-       }
-    }
-       end--;
     }
 
-    cout<< (start - end - 1) + s.size() -1 - start << endl;
+    long long index2 = 0;
+    long long reduced = 0;
+    if(flag){
+        index2 = func(s, index1, '0', '5');
+        reduced = s.length() - index2 - 2;
+    }
+    else{
+        index2 = func(s, index1, '2', '7'); 
+        long long ans = funcA(s);
+        reduced = s.length() - index2 - 2;
+        reduced = min(reduced, ans);
+    }
+
+    cout << reduced << endl;
 }
 
 int main() {
