58a14e0e2d0e2387d627e2fb6c0e99fa9d46ab02
Sudhanshi Garg <sudhanshigarg13@gmail.com>
2026-04-25 16:17:15 +0530

    1607B

--- 1606A.cpp
+++ 1606A.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 using namespace std;
 
-string solve1(string s, string t){
+string solve1(string s){
     s[0] = s[s.length() - 1];
     return s;
 }
 
 void solve() {
     string s; cin >> s;
-    cout << solve1 << endl;
+    cout << solve1(s) << endl;
 }
 
 int main() {
