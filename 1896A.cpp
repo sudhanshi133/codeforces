e57a86cdad637524b85bc18a078e3f233ed82137
Sudhanshi Garg <sudhanshigarg13@gmail.com>
2026-04-26 13:29:25 +0530

    1896A

--- 1896A.cpp
+++ 1896A.cpp
@@ -11,29 +11,7 @@ cin>>ele;
 }
 
 bool flag = true;
-int a = vec[0];
 
-for(int i = 1 ; i < n-1 ; i +=2){
-    int b = vec[i];
-    int c = vec[i+1];
- if(b > a && c > b){
-    a = vec[i];
-    continue;
- }
-else if (b > a && b > c){
-    if(c > a){
-        a = vec[i];
-    }
-    else{
-        flag = !flag;
-        break;
-    }
-}
-else{
-    flag = !flag;
-    break;
-}
-}
 
 if(flag){
 cout << "YES"<< endl;
