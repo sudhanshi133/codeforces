5ddc2865452db4cef213810fb0102c0aa98ecc3f
Sudhanshi Garg <sudhanshigarg13@gmail.com>
2026-04-30 22:59:47 +0530

    1807D

--- 1878C.cpp
+++ 1878C.cpp
@@ -1,19 +1,21 @@
 #include <iostream>
 using namespace std;
 
+bool func(long long n, long long k, long long x){
+    long long sum = (k*(k+1))/2;
+    if(sum > x) return false;
+    long long b = n - k;
+    sum = (n*(n+1)/2) - (b*(b+1)/2);
+    if(x>sum) return false;
+    return true;
+}
+
 void solve() {
-long long int n,k,x;
+long long n,k,x;
 cin >> n >> k >> x;
 
-if( k==x && k!=1){
-    cout << "NO" << endl;
-    return;
-} 
-long long int sum = (n*(n+1))/2;
-
-    if(sum < x) cout << "NO" << endl;
-    else cout << "YES" << endl;
-
+if(func(n,k,x)) cout << "YES\n";
+else cout << "NO\n";
 }
 
 int main() {
