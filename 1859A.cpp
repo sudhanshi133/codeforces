cccb51544bb1679dd13ace556ca35671e3994f99
Sudhanshi Garg <sudhanshigarg13@gmail.com>
2026-05-01 14:36:46 +0530

    1859A

--- 1859A.cpp
+++ 1859A.cpp
@@ -3,18 +3,58 @@
 #include<algorithm>
 using namespace std;
 
-void solve() {
-    int n; cin >> n;
-    vector<int> vec(n);
-
-    for(auto &ele : vec){
-        cin >> ele;
+void func(vector<int>& vec){
+    sort(vec.begin(), vec.end());
+    int n = vec.size();
+    if(vec[n-1] == vec[0]){
+        cout << -1 << endl;
+        return; 
     }
-    
-    return;
+
+    int k = 1;
+    for(int i=n-2; i>=0; i--){
+        if(vec[i] == vec[n-1]) k++;
+        else break;
+    }
+    cout << n-k << " " << k << endl;
+
+    for(int i=0; i<n; i++){
+        cout << vec[i] << " ";
+        if(i==n-k-1) cout << endl;
+    }
+    cout << endl;
+}
+
+void solve() {
+int n; cin >> n;
+int odds=0, evens=0;
+vector<int> vec(n);
+
+for(auto &ele : vec){
+    cin >> ele;
+    if(ele%2!=0) odds++;
+    else evens++;
+}
+
+if(odds==0 || evens==0){
+    func(vec);
+}
+else{
+    cout << odds << " " << evens << endl;
+    for(int i=0; i<n; i++){
+        if(vec[i]%2!=0) cout << vec[i] << " ";
+    }
+    cout << endl;
+
+    for(int i=0; i<n; i++){
+        if(vec[i]%2==0) cout << vec[i] << " ";
+    }
+    cout << endl;
+}
+return;
 }
 
 int main() {
-    int t; cin >> t;
-    while (t--) solve();
+int t; cin >> t;
+while (t--) solve();
 }
