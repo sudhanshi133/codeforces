5c229d1689af3e493f6453f8e0e7bf8ea1c2f9ea
Sudhanshi Garg <sudhanshigarg13@gmail.com>
2026-05-02 12:01:53 +0530

    1607B

--- 1607B.cpp
+++ 1607B.cpp
@@ -2,7 +2,25 @@
 using namespace std;
 
 void solve() {
-    int n; cin >> n;
+    long long x,n;
+    cin >> x >> n;
+
+    long long left = n%4;
+    long long divisor = n/4;
+    long long ans = x;
+
+    int count = 1;
+    while(left--){
+        long long b = (divisor * 4) + count;
+        if(ans%2==0) b = -1*b;
+        ans += b;
+        count ++;
+    }
+
+
+
+   cout << ans << "\n";
+    return;
 }
 
 int main() {
