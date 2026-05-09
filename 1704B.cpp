19a6918bf791f91e4a8e1c873120568b802ee19f
Sudhanshi Garg <sudhanshigarg13@gmail.com>
2026-05-09 10:48:49 +0530

    starting 1000 rating problems

---
+++ 1704B.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+void solve() {
+    int n; cin >> n;
+    int x; cin >> x;
+    vector<int> vec(n);
+
+    for(auto &ele : vec){
+        cin >> ele;
+    }
+
+    
+    return;
+}
+
+int main() {
+    int t; cin >> t;
+    while (t--) solve();
+}
