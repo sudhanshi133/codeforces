f288c8513ed2d3bf20f301b4efa0f3ab1c685924
Sudhanshi Garg <sudhanshigarg13@gmail.com>
2026-05-09 19:17:50 +0530

    starting 1000 rating problems

--- 1725B.cpp
+++ 1725B.cpp
@@ -3,18 +3,39 @@
 #include<algorithm>
 using namespace std;
 
+bool func(int required, int i, int j){
+    return required <= j-i+1;
+}
+
 void solve() {
     int n; cin >> n;
+    int d; cin >> d;
     vector<int> vec(n);
 
     for(auto &ele : vec){
         cin >> ele;
     }
-    
+
+    int ans = 0;
+   int i = 0;
+    sort(vec.begin(), vec.end());
+    if(vec[0] > d){
+        cout << n << endl;
+        return;
+    }
+
+    for(int j=n-1;j>=0 && i<j; j--){
+        int req  = d/vec[j] + 1;
+            if(func(req, i, j)) i+= (req-1);
+            else break;
+            ans++;
+    }
+
+    cout << ans;
+    cout << endl;
     return;
 }
 
 int main() {
-    int t; cin >> t;
-    while (t--) solve();
+    solve();
 }
