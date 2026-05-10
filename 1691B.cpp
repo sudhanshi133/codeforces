6bcd38de93b2e2ba29c1d0741bc460a55cb5a07d
Sudhanshi Garg <sudhanshigarg13@gmail.com>
2026-05-10 14:28:15 +0530

    starting 1000 rating problems

--- 1691B.cpp
+++ 1691B.cpp
@@ -2,6 +2,22 @@
 #include<vector>
 #include<algorithm>
 using namespace std;
+int func(vector<int>& ans, int index, vector<int> vec){
+    int b = 1;
+    for(int i=index+1; i<vec.size(); i++){
+        if(vec[i] != vec[index]) break;
+        b++;
+    }
+
+    int k = b - 1;
+    if(b==1) return -1;
+
+    for(int i = index + b; i >= index + 1; i--){
+        ans.push_back(i);
+    }
+
+    return b;
+}
 
 void solve() {
     int n; cin >> n;
@@ -10,7 +26,24 @@ void solve() {
     for(auto &ele : vec){
         cin >> ele;
     }
+
+    int index = 0;
+    vector<int> ans;
     
+    while(index < vec.size()){
+        int b = func(ans, index, vec);
+        if(b == -1){
+            cout << -1 << endl;
+            return;
+        }
+        index += b;
+    }
+
+    for(int i=0; i<vec.size(); i++){
+        cout << ans[i] << " ";
+    }
+
+    cout << endl;
     return;
 }
 
