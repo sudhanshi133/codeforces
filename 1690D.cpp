95b9aedf573d37e9d44fca68fe37d6de8663c918
Sudhanshi Garg <sudhanshigarg13@gmail.com>
2026-05-10 12:34:47 +0530

    starting 1000 rating problems

--- 1690D.cpp
+++ 1690D.cpp
@@ -3,14 +3,37 @@
 #include<algorithm>
 using namespace std;
 
+vector<int> func(string s, int x){
+    vector<int> ans;
+    for(int i=0; i<= s.length() - x; i++){
+        if(s[i] == 'B')  ans.push_back(i);
+    }
+    return ans;
+}
+
+int calc(int index, string s, int x){
+    
+}
+
 void solve() {
     int n; cin >> n;
-    vector<int> vec(n);
+    int x; cin >> x;
+    string s; cin >> s; 
 
-    for(auto &ele : vec){
-        cin >> ele;
+    vector<int> black = func(s, x);
+    int ans = INT_MAX;
+    int k = -1;
+    sort(black.begin(), black.end());
+
+    for(int i=0; i < black.size(); i++){
+        if(k+1 == black[i] && i!=0) continue;
+        int len = calc(i, s, x);
+        ans = min(ans, len);
+        k = black[i];
     }
-    
+
+   cout << ans;
+  cout << endl;
     return;
 }
 
