0649837d9409fef61f6b601c6cb136f5ef349a02
Sudhanshi Garg <sudhanshigarg13@gmail.com>
2026-05-14 02:17:42 +0530

    1438

--- 1831B.cpp
+++ 1831B.cpp
@@ -7,18 +7,26 @@ void solve() {
     int n; cin >> n;
     vector<int> arr1(n);
     vector<int> arr2(n);
+    vector<int> vec1(2*n);
+    vector<int> vec2(2*n);
 
-    for(auto &ele : arr1){
-        cin >> ele;
+    for(int i=0; i<n; i++){
+        cin >> arr1[i];
+        vec1[arr1[i]]++;
     }
 
-    for(auto &ele : arr2){
-        cin >> ele;
+  for(int i=0; i<n; i++){
+        cin >> arr2[i];
+        vec2[arr2[i]]++;
     }
     
-    int ans = 0;
+    int ans = 1;
 
-    cout << ans << endl;
+    for(int i=0; i<2*n; i++){
+       if(vec1[i] > 0 && vec2[i] > 0)  ans = max(ans, vec1[i] + vec2[i]);
+    }
+
+   cout << ans << endl;
     return;
 }
 
