964696333b1e40388d0d750186c350461e2c9a0b
sudhanshi133 <sudhanshigarg13@gmail.com>
2026-05-16 20:41:54 +0530

    adding contest problems

--- 2228B.cpp
+++ 2228B.cpp
@@ -1,20 +1,16 @@
 #include <iostream>
-#include<vector>
-#include<algorithm>
 using namespace std;
 
 void solve() {
     long long n, x1, x2, k;
     cin >> n >> x1 >> x2 >> k;
 
-    long long leftEnd  = ((x2-k-1)%n+n)%n+1;  
-    long long rightEnd = ((x2+k-1)%n)%n+1;    
+    long long diff = abs(x1 - x2);
+    long long a = diff;         
+    long long b = n - diff;       
 
-    long long distLeft  = min(abs(x1 - leftEnd),  n - abs(x1 - leftEnd));
-    long long distRight = min(abs(x1 - rightEnd), n - abs(x1 - rightEnd));  
-
-    
-return;
+    cout << min(a,b) + k;
+    cout << endl;
 }
 
 int main() {
