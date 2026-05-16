6cbd890d4157ed0688c57a23990ddb321b9911b9
Sudhanshi Garg <sudhanshigarg13@gmail.com>
2026-05-16 14:45:00 +0530

    ques added

--- 1614B.cpp
+++ 1614B.cpp
@@ -7,14 +7,40 @@ void solve() {
     int n; cin >> n;
     vector<int> vec(n);
     vector<int> ans(n+1);
+    ans[0] = 0;
     int minDis = 0;
 
     for(auto &ele : vec){
         cin >> ele;
     }
 
-    
+    vector<pair<int,int>> nums(n);
+    for(int i = 0; i < n; i++) nums[i] = {vec[i], i};
+    sort(nums.begin(), nums.end(), greater<pair<int,int>>());
+
+
+    int pos = 1;
+    int neg = -1;
+    bool flag = true;
+
+for(auto &p : nums){
+    int a = p.first;
+    int b = p.second + 1;
+    if(flag) {
+        ans[b] = pos;
+        minDis += 2 * a * pos; 
+        pos++;
+    }
+    else{
+        ans[b] = neg;
+        minDis += 2 * a * abs(neg); 
+         neg--;
+    }
+    flag = !flag;
+}
+
     cout << minDis << endl;
+
     for(int i=0; i<=n; i++){
         cout << ans[i] << " ";
     }
