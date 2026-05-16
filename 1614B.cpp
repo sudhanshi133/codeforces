f5ad3a981cfe74d15e6de10cc916f74e22b9c3ac
Sudhanshi Garg <sudhanshigarg13@gmail.com>
2026-05-16 14:48:23 +0530

    ques added

--- 1614B.cpp
+++ 1614B.cpp
@@ -5,39 +5,38 @@ using namespace std;
 
 void solve() {
     int n; cin >> n;
-    vector<int> vec(n);
-    vector<int> ans(n+1);
+    vector<long long> vec(n);
+    vector<long long> ans(n+1);
     ans[0] = 0;
-    int minDis = 0;
+    long long minDis = 0;
 
     for(auto &ele : vec){
         cin >> ele;
     }
 
-    vector<pair<int,int>> nums(n);
-    for(int i = 0; i < n; i++) nums[i] = {vec[i], i};
-    sort(nums.begin(), nums.end(), greater<pair<int,int>>());
+    vector<pair<long long,long long>> nums(n);
+    for(long long i = 0; i < n; i++) nums[i] = {vec[i], i};
+    sort(nums.begin(), nums.end(), greater<pair<long long,long long>>());
 
-
-    int pos = 1;
-    int neg = -1;
+    long long pos = 1;
+    long long neg = -1;
     bool flag = true;
 
-for(auto &p : nums){
-    int a = p.first;
-    int b = p.second + 1;
-    if(flag) {
-        ans[b] = pos;
-        minDis += 2 * a * pos; 
-        pos++;
+    for(auto &p : nums){
+        long long a = p.first;
+        long long b = p.second + 1;
+        if(flag) {
+            ans[b] = pos;
+            minDis += 2 * a * pos;
+            pos++;
+        }
+        else{
+            ans[b] = neg;
+            minDis += 2 * a * abs(neg);
+            neg--;
+        }
+        flag = !flag;
     }
-    else{
-        ans[b] = neg;
-        minDis += 2 * a * abs(neg); 
-         neg--;
-    }
-    flag = !flag;
-}
 
     cout << minDis << endl;
 
