f87659ea46aba90fb79037bcda41fbd609db46e5
sudhanshi133 <sudhanshigarg13@gmail.com>
2026-05-17 12:59:18 +0530

    adding contest problems

--- 1474B.cpp
+++ 1474B.cpp
@@ -4,19 +4,27 @@
 using namespace std;
 
 
-long long func(long long i, int n){
+bool isPrime(long long n){
+    if(n < 2) return false;
+    for(int i = 2; i*i <= n; i++)
+        if(n % i == 0) return false;
+    return true;
+}
 
+int func(long long n){
+    while(!isPrime(n)) n++;
+    return n;
 }
 
 void solve() {
     int n; cin >> n;
     int b = n+1;
     
-    long long ans = b;
-    bool flag = true;
-    long long i = b+n;
-  
-    ans *= func(i, n);
+    long long p = func(n + 1); 
+    long long i = p + n;  
+    long long ans = func(i) * p;
+    
+
     cout << ans << endl;
     return;
 }
