bb544a00043441e3f51db0dd65960142498bac02
sudhanshi133 <sudhanshigarg13@gmail.com>
2026-05-18 20:15:53 +0530

    adding contest problems

---
+++ 2230A.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+void solve() {
+    long long n, a, b;
+    cin >> n >> a >> b;
+    long long ans = 0;
+
+    if(n==1){
+        ans = min(a,b);
+    }
+    else if(a*3 <= b){
+        ans = n * a;
+    }
+    else{
+        int remainder = n%3;
+        ans+= (n/3) * b;
+        if(remainder == 1){
+            ans += a;
+        }
+        else if(remainder == 2) {
+            if(a*2 > b) ans += b;
+            else ans += a*2;
+        }
+    }
+
+    cout << ans << endl;
+    return;
+}
+
+int main() {
+    int t; cin >> t;
+    while (t--) solve();
+}
