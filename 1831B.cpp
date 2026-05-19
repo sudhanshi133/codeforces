b2ca3b75e2f2c48999d8512e4e771da04feab263
sudhanshi133 <sudhanshigarg13@gmail.com>
2026-05-19 10:36:27 +0530

    adding ques

--- 1831B.cpp
+++ 1831B.cpp
@@ -3,30 +3,41 @@
 #include<algorithm>
 using namespace std;
 
+vector<int> getMaxFreq(vector<int>& arr, int n) {
+    vector<int> freq(2 * n + 1, 0);
+    int i = 0;
+    while (i < n) {
+        int x = arr[i];
+        int count = 0;
+        while (i < n && arr[i] == x) {
+            count++;
+            i++;
+        }
+        freq[x] = max(freq[x], count);
+    }
+    return freq;
+}
+
 void solve() {
     int n; cin >> n;
     vector<int> arr1(n);
     vector<int> arr2(n);
-    vector<int> vec1(2*n);
-    vector<int> vec2(2*n);
 
     for(int i=0; i<n; i++){
         cin >> arr1[i];
-        vec1[arr1[i] - 1]++;
     }
 
   for(int i=0; i<n; i++){
         cin >> arr2[i];
-        vec2[arr2[i] - 1]++;
     }
     
     int ans = 1;
+    vector<int> res1 = getMaxFreq(arr1, n);
+    vector<int> res2 = getMaxFreq(arr2, n);
 
-    for(int i=0; i<2*n; i++){
-       // cout << vec1[i] << " " << vec2[i] << endl;
-      if(vec1[i] > 0 && vec2[i] > 0)  ans = max(ans, vec1[i] + vec2[i]);
+    for(int i=0; i<2*n+1; i++){
+        ans = max(ans, res1[i] + res2[i]);
     }
-   // cout << endl;
 
    cout << ans << endl;
     return;
