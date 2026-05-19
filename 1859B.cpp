b44077dfc08df843195aaa23a6e9048043808715
sudhanshi133 <sudhanshigarg13@gmail.com>
2026-05-20 00:48:55 +0530

    adding problems

---
+++ 1859B.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+void solve() {
+    int n; cin >> n;
+    //vector<int> first;
+    vector<int> second;
+    int mini = INT_MAX;
+
+    while(n--){
+        int k;
+        cin >> k;
+        vector<int> vec(k);
+        for(auto &ele : vec) cin >> ele;
+        sort(vec.begin(), vec.end());
+       // first.push_back(vec[0]);
+        mini = min(mini, vec[0]);
+        second.push_back(vec[1]);
+    }
+
+    long long ans = 0;
+    sort(second.begin(), second.end());
+    ans += mini;
+    
+    for(int i=1; i<second.size(); i++){
+        ans += second[i];
+    }
+
+    cout << ans << endl;
+    return;
+}
+
+int main() {
+    int t; cin >> t;
+    while (t--) solve();
+}
