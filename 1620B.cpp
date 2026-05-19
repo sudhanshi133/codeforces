ce3bef0fd71fbf41f28af6cd38d0fc5743ce8ab4
sudhanshi133 <sudhanshigarg13@gmail.com>
2026-05-19 23:10:09 +0530

    adding problems

--- 1620B.cpp
+++ 1620B.cpp
@@ -4,50 +4,32 @@
 using namespace std;
 
 void solve() {
-    int w, h; cin >> w >> h;
+    long long w, h; cin >> w >> h;  // read as long long
 
     int a; cin >> a;
-    vector<int> vec1(a);
+    vector<long long> vec1(a);
+    for(auto &ele : vec1) cin >> ele;
 
-    for(auto &ele : vec1){
-       cin >> ele;
-    }
-
-    int diff = (vec1[a-1] - vec1[0]) * w;
-
+    long long diff = (vec1[a-1] - vec1[0]) * h;  // bottom side, height = h
 
     int b; cin >> b;
-    vector<int> vec2(b);
-    
-    for(auto &ele : vec2){
-       cin >> ele;
-    }
-
-    int j = (vec2[b-1] - vec2[0]) * w;
-    diff = max(diff, j);
+    vector<long long> vec2(b);
+    for(auto &ele : vec2) cin >> ele;
+    diff = max(diff, (vec2[b-1] - vec2[0]) * h);  // top side, height = h
 
     int c; cin >> c;
-    vector<int> vec3(c);
-    
-    for(auto &ele : vec3){
-       cin >> ele;
-    }
-
-   diff = max(diff, (vec3[c-1] - vec3[0]) * h);
+    vector<long long> vec3(c);
+    for(auto &ele : vec3) cin >> ele;
+    diff = max(diff, (vec3[c-1] - vec3[0]) * w);  // left side, height = w
 
     int d; cin >> d;
-    vector<int> vec4(d);
-    
-    for(auto &ele : vec4){
-       cin >> ele;
-    }
+    vector<long long> vec4(d);
+    for(auto &ele : vec4) cin >> ele;
+    diff = max(diff, (vec4[d-1] - vec4[0]) * w);  // right side, height = w
 
-   diff = max(diff, (vec4[d-1] - vec4[0]) * h);
-
-    cout << diff ;
-    cout << endl;
-    return;
+    cout << diff << "\n";
 }
+
 
 int main() {
     int t; cin >> t;
