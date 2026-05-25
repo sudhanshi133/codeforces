c76816c526d579f2d961924316d7c05234623bd4
sudhanshi133 <sudhanshigarg13@gmail.com>
2026-05-25 11:04:20 +0530

    adding problems

--- 1850E.cpp
+++ 1850E.cpp
@@ -4,13 +4,20 @@
 using namespace std;
 
 
-long long func(long long mid, vector<long long>& vec, long long x){
-   
+long long area(long long w, vector<long long>& vec, long long c){
+    long long sum = 0;
+    for (long long s : vec) {
+        long long side = s + 2 * w;
+        sum += side * side;
+
+    if (sum > c) return sum; 
+    }
+    return sum;
 }
 
 void solve() {
     int n; cin >> n;
-    long long x; cin >> x;
+    long long c; cin >> c;
     vector<long long> vec(n);
 
     for(auto &ele : vec){
@@ -21,16 +28,22 @@ void solve() {
     long long end = 2e9;
     long long ans;
  
-    while(start<=end){
-        int mid = start + (end-start)/2;
-        if(func(mid, vec, x)){
+    while (start <= end) {
+        long long mid = start + (end - start) / 2;
+        long long val = area(mid, vec, c);
+
+        if (val == c) {
             ans = mid;
+            break;
+        } 
+        else if (val < c) {
+            start = mid + 1;
+        } 
+        else {
             end = mid - 1;
         }
-        else {
-            start = mid + 1;
-        }
     }
+
 
     cout << ans << endl;
     return;
