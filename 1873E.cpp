154ff84920b1724ebe39295189d4ead823ac98dc
sudhanshi133 <sudhanshigarg13@gmail.com>
2026-05-25 10:27:14 +0530

    adding problems

---
+++ 1873E.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+bool possible(long long h, vector<int>& a, long long x) {
+    long long water = 0;
+    for (int i = 0; i < a.size(); i++) {
+        if (a[i] < h) {
+            water += (h - a[i]);
+            if (water > x) return false;
+        }
+    }
+    return true;
+}
+
+void solve() {
+    int n;
+    long long x;
+    cin >> n >> x;
+
+    vector<int> a(n);
+    for (auto &v : a) cin >> v;
+
+    long long low = 1, high = 2e9, ans = 1;
+
+    while (low <= high) {
+        long long mid = low + (high - low) / 2;
+
+        if (possible(mid, a, x)) {
+            ans = mid;
+            low = mid + 1;
+        } else {
+            high = mid - 1;
+        }
+    }
+
+    cout << ans << '\n';
+}
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) solve();
+    return 0;
+}
