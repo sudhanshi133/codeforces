a173308433211ecf28151447a23844944a8799e0
sudhanshi133 <sudhanshigarg13@gmail.com>
2026-05-27 00:56:15 +0530

    adding ques

--- 1610B.cpp
+++ 1610B.cpp
@@ -3,6 +3,23 @@
 #include<algorithm>
 using namespace std;
 
+
+bool check(vector<int>& vec, int n, int skip){
+    int i=0, j=n-1;
+    while(i < j){
+        if(vec[i] == vec[j]){ i++; j--; }
+        else if(vec[i] == skip) i++;
+        else if(vec[j] == skip) j--;
+        else return false;
+    }
+    return true;
+}
+
+bool func(int i, int j, vector<int>& vec, int n){
+    if(i >= j) return true;
+    return check(vec, n, vec[i]) || check(vec, n, vec[j]);
+}
+
 void solve() {
     int n; cin >> n;
     vector<int> vec(n);
@@ -11,6 +28,18 @@ void solve() {
         cin >> ele;
     }
     
+    int i=0, j=n-1;
+
+    while(i<j){
+        if(vec[i] != vec[j]) break;
+        i++;
+        j--;
+    }
+
+   bool flag = func(i,j, vec, n);
+
+   if(flag) cout << "YES" << endl;
+    else cout << "NO" << endl;
     return;
 }
 
