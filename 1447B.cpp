49509712e23ebed80e1720b57d588600442571dd
sudhanshi133 <sudhanshigarg13@gmail.com>
2026-05-30 16:49:07 +0530

    adding ques

--- 1447B.cpp
+++ 1447B.cpp
@@ -1,20 +1,38 @@
 #include <iostream>
-#include<vector>
-#include<algorithm>
+#include <cmath>
 using namespace std;
 
 void solve() {
-    int n; cin >> n;
-    vector<int> vec(n);
-
-    for(auto &ele : vec){
-        cin >> ele;
-    }
+    int n, m; 
+    cin >> n >> m; 
     
-    return;
+    int arr[10][10];
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < m; j++)
+            cin >> arr[i][j];
+
+    int negCount = 0;   // count of negative numbers
+    int minAbs = INT_MAX; // smallest absolute value
+    long long sum = 0;
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            if(arr[i][j] < 0) negCount++;
+            minAbs = min(minAbs, abs(arr[i][j]));
+            sum += abs(arr[i][j]);
+        }
+    }
+
+    // if odd negatives, subtract 2*minAbs
+    if(negCount % 2 != 0)
+        sum -= 2 * minAbs;
+
+    cout << sum << "\n";
 }
 
-int main() {
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
     int t; cin >> t;
-    while (t--) solve();
+    while(t--) solve();
 }
