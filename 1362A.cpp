cfe112a4498c51dd85a0e20ae871aeb6bed53366
sudhanshi133 <sudhanshigarg13@gmail.com>
2026-05-30 15:37:12 +0530

    adding ques

--- 1362A.cpp
+++ 1362A.cpp
@@ -3,14 +3,53 @@
 #include<algorithm>
 using namespace std;
 
-void solve() {
-    int n; cin >> n;
-    vector<int> vec(n);
 
-    for(auto &ele : vec){
-        cin >> ele;
+// first one is bigger num
+void func(long long a, long long b){
+    long long c = 0;
+    if(a>b) c = a/b;
+    else c = b/a;
+
+    if(a>b && a%b!=0){
+        cout << -1 << endl;
+        return;
     }
-    
+    else if(a<b && b%a!=0){
+        cout << -1 << endl;
+        return;
+    }
+
+    if(c%2!=0){
+        cout << -1 << endl;
+        return;
+    }
+
+    long long ans = 0;
+
+    while(c>1){
+        if(c%8==0) c /= 8;
+        else if(c%4==0) c/= 4;
+        else if(c%2==0) c/=2;
+
+        else{
+            cout << -1 << endl;
+            return;
+        }
+        ans++;
+    }
+
+    cout << ans << endl;
+}
+
+void solve() {
+    long long a, b;
+    cin >> a >> b;
+    if(a==b){
+        cout << 0 << endl;
+        return;
+    }
+
+    func(a,b);
     return;
 }
 
