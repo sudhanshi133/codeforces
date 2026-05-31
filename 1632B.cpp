ae791c2c3ca44a3c414ca6d57d547a5a82c5dc36
sudhanshi133 <sudhanshigarg13@gmail.com>
2026-05-31 21:41:53 +0530

    adding ques

--- 1632B.cpp
+++ 1632B.cpp
@@ -1,16 +1,9 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 void solve(){
     int n; cin >> n;
-
-    int p = 1;
-    while(p * 2 <= n-1) p *= 2;
-    cout << p << " " << 0 << " ";
-
-    for(int i = 1; i < n; i++){
-        if(i != p) cout << i << " ";
-    }
     cout << "\n";
 }
 
