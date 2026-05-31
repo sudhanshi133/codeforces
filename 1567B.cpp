1de2bd9fbefa21fa092d20053569845518f32fb3
sudhanshi133 <sudhanshigarg13@gmail.com>
2026-05-31 21:29:18 +0530

    adding ques

--- 1567B.cpp
+++ 1567B.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-#include<vector>
-#include<algorithm>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 long long xorUpTo(long long n){
@@ -11,17 +11,31 @@ long long xorUpTo(long long n){
 }
 
 void solve() {
-   int a, b;
-   cin >> a >> b;
+    long long a, b;
+    cin >> a >> b;
 
-   long long result = xorUpTo(a - 1); 
-   long long ans = a;
+    long long result = xorUpTo(a - 1);  // XOR of 0 to a-1
 
-   if(result == b) cout << ans << endl;
-   else 
+    if(result == b){
+        // XOR already matches!
+        cout << a << "\n";
+    }
+    else {
+        long long x = result ^ b;  // extra element needed
+        if(x != a){
+            // x can be added safely
+            cout << a + 1 << "\n";
+        }
+        else {
+            // x == a, can't add! need 2 extra elements
+            cout << a + 2 << "\n";
+        }
+    }
 }
 
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
     int t; cin >> t;
     while (t--) solve();
 }
