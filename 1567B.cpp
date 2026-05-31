c7f5c54d7eab80eb0d932d8bbf356ef995ce9697
sudhanshi133 <sudhanshigarg13@gmail.com>
2026-05-31 21:19:21 +0530

    adding ques

--- 1567B.cpp
+++ 1567B.cpp
@@ -3,12 +3,22 @@
 #include<algorithm>
 using namespace std;
 
+long long xorUpTo(long long n){
+    if(n % 4 == 0) return n;
+    if(n % 4 == 1) return 1;
+    if(n % 4 == 2) return n + 1;
+    return 0;
+}
+
 void solve() {
    int a, b;
    cin >> a >> b;
-   
-   
-    return;
+
+   long long result = xorUpTo(a - 1); 
+   long long ans = a;
+
+   if(result == b) cout << ans << endl;
+   else 
 }
 
 int main() {
