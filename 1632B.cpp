17cc0a4676e9c1e49a97a5a8c026bfdfbe63b36f
sudhanshi133 <sudhanshigarg13@gmail.com>
2026-05-31 21:56:36 +0530

    adding ques

--- 1632B.cpp
+++ 1632B.cpp
@@ -4,7 +4,16 @@ using namespace std;
 
 void solve(){
     int n; cin >> n;
-    cout << "\n";
+
+    int k =0;
+    while((1<<(k+1)) <= n-1) k++;
+
+    for(int i=(1<<k)-1; i>=0; i--)
+    cout << i << " ";
+    for(int i=(1<<k); i<n; i++)
+    cout << i << " ";
+
+    cout << endl;
 }
 
 int main(){
